a6191b17ef6fd7c92871d2deb401008f77d18623
Aleferreirao <ale_weiccy@hotmail.com>
2025-10-09 21:54:42 -0300

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,16 +1,49 @@
-import sqlite3
+import mysql.connector
 
-DB_NAME = "oficina.db"  # Nome do banco adaptado para projeto 
+#Dados do servidor MySQL
+HOST = "localhost"
+USER = "root"
+PASSWORD = "senac"
+DB_NAME = "escola_db" #nome do banco de dados
 
-def conectar():
-    return sqlite3.connect(DB_NAME)
+def conectar(usando_banco=True):
+    """Conectar ao Mysql 
+    -usando_banco=True -> conectar direto ao schema DB_NAME
+    -usando_banco=False -> conectar sem definir um banco"""
+    if usando_banco:
+        return mysql.connector.connect(
+            host = HOST,
+            user = USER,
+            port = "3306",
+            password = PASSWORD,
+            database = DB_NAME
+        )
+    else:
+        return mysql.connector.connect(
+            host = HOST,
+            user = USER,
+            port="3306",
+            password = PASSWORD
+        ) 
+
+def criar_banco():
+    #Criar banco caso não exista
+    conn = conectar(usando_banco=False) #Cria uma instância de conexão com o banco
+    cursor = conn.cursor() # Aponta para o Banco de Dadosa
+    cursor.execute(""" CREATE DATABASE 
+                   IF NOT EXISTS {DB_NAME}""")
+    conn.commit() # salva as alterações no banco
+    conn.close() # fecha a conexão com o banco
+
 
 def criar_tabelas():
-    con = conectar()
-    cur = con.cursor()
-
+    #cria as tabelas (curso, turma, aluno) 
+    # se não existirem do BD
+    conn = conectar() # abre a conexão com o banco
+    cursor = conn.cursor() # cria um cursor que aponta pro BD e executa intruções SQL
+    
     # Tabela de clientes
-    cur.execute("""
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS clientes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nome TEXT NOT NULL,
@@ -21,7 +54,7 @@ def criar_tabelas():
     """)
 
     # Tabela de veículos
-    cur.execute("""
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS veiculos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             cliente_id INTEGER NOT NULL,
@@ -35,7 +68,7 @@ def criar_tabelas():
     """)
 
     # Tabela de manutenções
-    cur.execute("""
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS manutencoes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             veiculo_id INTEGER NOT NULL,
@@ -48,5 +81,5 @@ def criar_tabelas():
         )
     """)
 
-    con.commit()
-    con.close()
+    conn.commit()
+    conn.close()
